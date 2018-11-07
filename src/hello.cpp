d3cbd9867bc5a1d3b051e0fab9c4e0964f5d6595
eduvazsal <edvzsal@hotmail.com>
2018-11-07 11:18:36 +0100

    More modifications to "Hello ROS"

--- src/hello.cpp
+++ src/hello.cpp
@@ -7,5 +7,5 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, "hello_ros");
 	ros::NodeHandle nh;
 
-	ROS_INFO_STREAM("Hello, ROS! I' trying Git now");
+	ROS_INFO_STREAM("Hello, ROS! I'm trying Git now :P");
 }
