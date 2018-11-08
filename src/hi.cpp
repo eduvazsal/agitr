036ca0d91986b813757ca14b4cb9fff82af9abef
eduvazsal <edvzsal@hotmail.com>
2018-11-08 12:06:23 +0100

    hi.cpp modified

--- src/hi.cpp
+++ src/hi.cpp
@@ -7,5 +7,5 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, "hi_ros");
 	ros::NodeHandle nh;
 
-	ROS_INFO_STREAM("Hi, ROS! I'm trying Git now :P");
+	ROS_INFO_STREAM("Hi, ROS! I'm trying Git branching now :P");
 }
